f6b192abae69c568eb076ec444d8f0242e34e352
marcosroman <marcosdromand@gmail.com>
2022-12-20 19:25:19 -0300

    basic ninja completed

---
+++ ninja.js
@@ -0,0 +1,26 @@
+class Ninja {
+	constructor(nombre) {
+		this.nombre=nombre;
+		this.salud=100; //?
+		this.velocidad=3;
+		this.fuerza=3;
+	}
+
+	sayName() {
+		console.log(this.name);
+	}
+
+	showStats() {
+			console.log(this);
+	}
+
+	drinkSake() {
+		this.salud+=10;
+	}
+}
+
+
+const ninja1 = new Ninja("Hyabusa");
+ninja1.sayName();
+ninja1.showStats();
+
